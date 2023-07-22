1ab6235212fd5ec6e00c9193d8beebe2464132e8
이종현 <ijonghyeon@ijonghyeon-ui-MacBookAir.local>
2023-07-23 05:42:27 +0900

    위젯 만들기

--- toonflix/lib/main.dart
+++ toonflix/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:toonflix/widgets/Button.dart';
 
 class Player {
   String? name; // ? => null 값을 가질 수 있음을 의미
@@ -6,13 +7,15 @@ class Player {
 }
 
 void main() {
-  var nico = Player(name: 'Jonghyun');
-  runApp(App());
+  // var nico = Player(name: 'Jonghyun');
+  runApp(const App());
 }
 
-// 위젯을 만든다는건 class 를 만든느것
+// 위젯을 만든다는건 class 를 만드는것
 // 위젯을 만들기 위해선 SDK를 상속 받아야함
 class App extends StatelessWidget {
+  const App({super.key});
+
   // StatelessWidget 상속 받기 위해선 build 메소드를 구현해줘야함
 
   @override
@@ -23,14 +26,81 @@ class App extends StatelessWidget {
     return MaterialApp(
       // Scaffold => 화면 구성과 구조에 관한 걸 다룰수 있는 위젯
       home: Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          elevation: 10,
-          title: Text('Hello! Flutter!'),
-        ),
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+        backgroundColor: const Color(0xFF181818),
+        body: Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 20,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const SizedBox(
+                  height: 80,
+                ),
+                // Row => 수평 , Column => 수직
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        const Text(
+                          'Hey Selena',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 28,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text('Welcome back',
+                            style: TextStyle(
+                              color: Colors.white.withOpacity(0.8),
+                              fontSize: 18,
+                            )),
+                      ],
+                    )
+                  ],
+                ),
+                const SizedBox(
+                  height: 120,
+                ),
+                Text(
+                  'Total Balance',
+                  style: TextStyle(
+                    fontSize: 22,
+                    color: Colors.white.withOpacity(0.8),
+                  ),
+                ),
+                const SizedBox(
+                  height: 5,
+                ),
+                const Text(
+                  '\$5 194 482',
+                  style: TextStyle(
+                    fontSize: 48,
+                    fontWeight: FontWeight.w600,
+                    color: Colors.white,
+                  ),
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Button(
+                        text: 'Transfer',
+                        bgColor: Color(0xFFF1B33B),
+                        textColor: Colors.black),
+                    Button(
+                        text: 'Request',
+                        bgColor: Color(0xFF1F2123),
+                        textColor: Colors.white)
+                  ],
+                )
+              ],
+            )),
       ),
     );
   }
